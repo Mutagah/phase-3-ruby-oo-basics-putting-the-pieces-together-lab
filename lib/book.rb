837bab63883f27deefddad27adb693b452a801aa
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-08-17 13:42:46 +0300

    creating classes combined with macros inside them

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :page_count, :title, :author, :genre
+    def initialize (title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
-
+first_book = Book.new("Introduction to Ruby")
+first_book.author = "Mutaga Kihahu"
+first_book.genre = "Programing learning"
+first_book.page_count = 695
+first_book.turn_page
